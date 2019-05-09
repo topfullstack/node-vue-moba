b70deda09409b46071384a43dc8876218c7a4bc8
wxs77577 <wu-xuesong@qq.com>
2019-05-09 18:26:32 +0800

    编辑分类

--- admin/src/views/CategoryList.vue
+++ admin/src/views/CategoryList.vue
@@ -4,26 +4,32 @@
     <el-table :data="items">
       <el-table-column prop="_id" label="ID" width="240"></el-table-column>
       <el-table-column prop="name" label="分类名称"></el-table-column>
+      <el-table-column fixed="right" label="操作" width="180">
+        <template slot-scope="scope">
+          <el-button type="text" size="small" 
+          @click="$router.push(`/categories/edit/${scope.row._id}`)">编辑</el-button>
+        </template>
+      </el-table-column>
     </el-table>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
       items: []
-    }
+    };
   },
   methods: {
-    async fetch(){
-      const res = await this.$http.get('categories')
-      this.items = res.data
+    async fetch() {
+      const res = await this.$http.get("categories");
+      this.items = res.data;
     }
   },
-  created(){
-    this.fetch()
+  created() {
+    this.fetch();
   }
-}
+};
 </script>
 
--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -6,9 +6,17 @@ module.exports = app => {
     const model = await Category.create(req.body)
     res.send(model)
   })
+  router.put('/categories/:id', async (req, res) => {
+    const model = await Category.findByIdAndUpdate(req.params.id, req.body)
+    res.send(model)
+  })
   router.get('/categories', async (req, res) => {
     const items = await Category.find().limit(10)
     res.send(items)
+  })
+  router.get('/categories/:id', async (req, res) => {
+    const model = await Category.findById(req.params.id)
+    res.send(model)
   })
   app.use('/admin/api', router)
 }

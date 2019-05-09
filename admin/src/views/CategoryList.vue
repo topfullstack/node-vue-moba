a8beb62cf54575dfbc1c5cd9473cd833fdf0af9c
wxs77577 <wu-xuesong@qq.com>
2019-05-09 18:24:15 +0800

    分类列表

--- admin/src/views/CategoryList.vue
+++ admin/src/views/CategoryList.vue
@@ -4,51 +4,26 @@
     <el-table :data="items">
       <el-table-column prop="_id" label="ID" width="240"></el-table-column>
       <el-table-column prop="name" label="分类名称"></el-table-column>
-      <el-table-column fixed="right" label="操作" width="180">
-        <template slot-scope="scope">
-          <el-button
-            type="text"
-            size="small"
-            @click="$router.push(`/categories/edit/${scope.row._id}`)"
-          >编辑</el-button>
-          <el-button type="text" size="small" @click="remove(scope.row)">删除</el-button>
-        </template>
-      </el-table-column>
     </el-table>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
+  data(){
     return {
       items: []
-    };
-  },
-  methods: {
-    async fetch() {
-      const res = await this.$http.get("categories");
-      this.items = res.data;
-    },
-    async remove(row) {
-      this.$confirm(`是否确定要删除分类 "${row.name}"`, "提示", {
-        confirmButtonText: "确定",
-        cancelButtonText: "取消",
-        type: "warning"
-      })
-        .then(async () => {
-          const res = await this.$http.delete(`categories/${row._id}`)
-          this.$message({
-            type: "success",
-            message: "删除成功!"
-          });
-          this.fetch()
-        })
     }
   },
-  created() {
-    this.fetch();
+  methods: {
+    async fetch(){
+      const res = await this.$http.get('categories')
+      this.items = res.data
+    }
+  },
+  created(){
+    this.fetch()
   }
-};
+}
 </script>
 

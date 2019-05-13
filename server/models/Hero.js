4bd52fb2d205edfe5108d1aaaa045398ac47f59b
wxs77577 <wu-xuesong@qq.com>
2019-05-13 21:20:09 +0800

    完成模型字段

--- server/models/Hero.js
+++ server/models/Hero.js
@@ -3,6 +3,29 @@ const mongoose = require('mongoose')
 const schema = new mongoose.Schema({
   name: { type: String },
   avatar: { type: String },
+  title: { type: String },
+  categories: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Category' }],
+  scores: {
+    difficult: { type: Number },
+    skills: { type: Number },
+    attack: { type: Number },
+    survive: { type: Number },
+  },
+  skills: [{
+    icon: { type: String },
+    name: { type: String },
+    description: { type: String },
+    tips: { type: String },
+  }],
+  items1: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Item' }],
+  items2: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Item' }],
+  usageTips: { type: String },
+  battleTips: { type: String },
+  teamTips: { type: String },
+  partners: [{
+    hero: { type: mongoose.SchemaTypes.ObjectId, ref: 'Hero' },
+    description: { type: String },
+  }],
 })
 
 module.exports = mongoose.model('Hero', schema)

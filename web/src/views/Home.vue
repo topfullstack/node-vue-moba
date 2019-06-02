0762b2d2a63340f4e920700fcd169c56519dfa08
wxs77577 <wu-xuesong@qq.com>
2019-06-02 21:17:23 +0800

    3.8

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -10,10 +10,21 @@
       <swiper-slide>
         <img class="w-100" src="../assets/images/210794580bb9303653804bb7b482f2a4.jpeg" alt>
       </swiper-slide>
-      <div class="swiper-pagination pagination-home text-right px-3 pb-1" 
-      slot="pagination">
-      </div>
+      <div class="swiper-pagination pagination-home text-right px-3 pb-1" slot="pagination"></div>
     </swiper>
+    <!-- end of swiper -->
+    <div class="nav-icons bg-white mt-3 text-center pt-3 text-dark-1">
+      <div class="d-flex flex-wrap">
+        <div class="nav-item mb-3" v-for="n in 10" :key="n">
+          <i class="sprite sprite-news"></i>
+          <div class="py-2">爆料站</div>
+        </div>
+      </div>
+      <div class="bg-light py-2 fs-sm">
+        <i class="sprite sprite-arrow mr-1"></i>
+        <span>收起</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -32,15 +43,26 @@ export default {
 </script>
 
 <style lang="scss">
-@import '../assets/scss/variables';
+@import "../assets/scss/variables";
 
 .pagination-home {
   .swiper-pagination-bullet {
     opacity: 1;
     border-radius: 0.1538rem;
-    background: map-get($colors, 'white');
+    background: map-get($colors, "white");
     &.swiper-pagination-bullet-active {
-      background: map-get($colors, 'info');
+      background: map-get($colors, "info");
+    }
+  }
+}
+.nav-icons {
+  border-top: 1px solid $border-color;
+  border-bottom: 1px solid $border-color;
+  .nav-item {
+    width: 25%;
+    border-right: 1px solid $border-color;
+    &:nth-child(4n) {
+      border-right: none;
     }
   }
 }

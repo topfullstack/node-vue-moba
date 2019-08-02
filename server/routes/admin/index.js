d4e34354ee53a88f117a1684ef51416efd8b6746
Johnny <wu-xuesong@qq.com>
2019-08-02 15:27:55 +0800

    fix upload url

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -47,7 +47,7 @@ module.exports = app => {
   const upload = multer({ dest: __dirname + '/../../uploads' })
   app.post('/admin/api/upload', authMiddleware(), upload.single('file'), async (req, res) => {
     const file = req.file
-    file.url = `http://localhost:3000/uploads/${file.filename}`
+    file.url = `http://test.topfullstack.com/uploads/${file.filename}`
     res.send(file)
   })
 
